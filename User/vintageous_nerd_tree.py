a5556061752d15ab17aac172bf358c81ec9a12b2
BjornMelgaard <melgaardbjorn@gmail.com>
2016-12-25 20:06:09 +0200

    just many

--- User/vintageous_nerd_tree.py
+++ User/vintageous_nerd_tree.py
@@ -1,10 +1,12 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+
 
 class NerdTreeCommand(sublime_plugin.WindowCommand):
     def run(self):
         was_visible = self.window.is_sidebar_visible()
         self.window.run_command("toggle_side_bar")
         if was_visible:
-          self.window.run_command("focus_group", { "group": 0 })
+            self.window.run_command("focus_group", {"group": 0})
         else:
-          self.window.run_command("focus_side_bar")
+            self.window.run_command("focus_side_bar")
