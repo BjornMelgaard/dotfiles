eb4f2859b77079c18e06022d0171b5a3265340ab
BjornMelgaard <melgaardbjorn@gmail.com>
2016-12-25 18:12:26 +0200

    own impl of mam

--- User/vintageous_mark_and_move.py
+++ User/vintageous_mark_and_move.py
@@ -29,11 +29,22 @@ class _vi_mark_and_move_space(ViTextCommandBase):
         super().__init__(*args, **kwargs)
 
     def run(self, edit, mode=None):
-        print(mode)
         if mode not in (modes.INTERNAL_NORMAL,
                         modes.VISUAL,
                         modes.VISUAL_LINE,
                         modes.VISUAL_BLOCK):
             raise ValueError('bad mode: ' + mode)
-        self.view.run_command('mark_and_move_do_it_all')
-        # self.enter_normal_mode(mode)
+
+        mark_and_move_marks = self.view.get_regions('mark_and_move')
+
+        def region_in(region, marks):
+            for mark in marks:
+                if mark.contains(region):
+                    return True
+            return False
+
+        if any(not region_in(region, mark_and_move_marks) for region in self.view.sel()):
+            self.view.run_command('mark_and_move_save')
+            self.enter_normal_mode(mode)
+        else:
+            self.view.run_command('mark_and_move_recall', {"clear": True})
