142a81a163041cd382c28c4e7576f6ebd6b581ec
BjornMelgaard <melgaardbjorn@gmail.com>
2016-12-25 17:51:03 +0200

    markandmove must not renter normal mode because it remove selection in markandmove somehow

--- User/vintageous_mark_and_move.py
+++ User/vintageous_mark_and_move.py
@@ -12,26 +12,28 @@ from Vintageous.vi.core import ViTextCommandBase
 class VistageousDoAll(ViOperatorDef):
     def __init__(self, *args, **kwargs):
         ViOperatorDef.__init__(self, *args, **kwargs)
-
         self.repeatable = False
         self.motion_required = False
 
     def translate(self, state):
-        return {
-          'action': '_vi_mark_and_move_space',
-          'action_args': {},
+        cmd = {}
+        cmd['action'] = '_vi_mark_and_move_space'
+        cmd['action_args'] = {
+            'mode': state.mode,
         }
+        return cmd
 
 
 class _vi_mark_and_move_space(ViTextCommandBase):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-  def run(self, edit, mode=None, count=1, motion=None):
-    if mode not in (modes.NORMAL,
-                    modes.VISUAL,
-                    modes.VISUAL_LINE,
-                    modes.VISUAL_BLOCK):
-        raise ValueError('bad mode: ' + mode)
-    self.view.run_command('mark_and_move_do_it_all')
-    self.enter_normal_mode(mode)
+    def run(self, edit, mode=None):
+        print(mode)
+        if mode not in (modes.INTERNAL_NORMAL,
+                        modes.VISUAL,
+                        modes.VISUAL_LINE,
+                        modes.VISUAL_BLOCK):
+            raise ValueError('bad mode: ' + mode)
+        self.view.run_command('mark_and_move_do_it_all')
+        # self.enter_normal_mode(mode)
