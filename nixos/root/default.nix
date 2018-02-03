8c6dcefe83a72e5f3e3710848198c2473cc8a216
Sergey Homa <melgaardbjorn@gmail.com>
2018-02-03 21:21:40 +0000

    Update default.nix

--- nixos/root/default.nix
+++ nixos/root/default.nix
@@ -6,25 +6,17 @@
     ./hardware-configuration.nix
     ./hardware-configuration-override.nix
     ./system.nix
-    ./fonts.nix
-    ./desktop.nix
-    ./system-packages.nix
+    #./hardware-programs.nix
+    #./fonts.nix
+    #./desktop.nix
+    #./system-packages.nix
+    #./programs.nix
     ];
 
   environment = {
     variables = {
       EDITOR="nvim";
     };
-  };
-
-  programs = {
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      enableAutosuggestions = true;
-      ohMyZsh.enable = true;
-    };
-    command-not-found.enable = true;
   };
 
   system.stateVersion = "17.09";
