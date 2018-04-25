a6e3857b1be60b2813e2cfd2f6e13a659e2fbb79
BjornMelgaard <melgaardbjorn@gmail.com>
2018-04-25 23:26:43 +0300

    feat: update

--- nixos/home/config.nix
+++ nixos/home/config.nix
@@ -5,41 +5,59 @@ with (import /etc/nixos/metaconfiguration.nix);
 {
   allowUnfree = true;
 
-  # packageOverrides = pkgs : with pkgs; {
+  packageOverrides = pkgs : with pkgs; {
 
-  #   # \curl -L https://get.rvm.io | bash -s stable --rails --auto-dotfiles
-  #   # gem install bundle
-  #   # gem install pg
-  #   # gem install nokogiri -- --use-system-libraries
-  #   # bundle config build.nokogiri --use-system-libraries
-  #   # bundle install
+    # TODO: impure python env
+    # pythonEnv = with pkgs; buildEnv {
+    #   name = "pythonEnv";
+    #   paths = [
+    #     (with python36Packages; python.buildEnv.override {
+    #         extraLibs = [
+    #           setuptools
+    #           jedi
+    #           flake8
+    #           numpy
+    #           isort
+    #           yapf
+    #           pytest
+    #         ];
+    #     })
+    #   ];
+    # };
 
-  #   rvmEnv = pkgs.myEnvFun {
-  #     name = "rvm";
-  #     buildInputs = [ stdenv pkgconfig python pythonPackages.boto procps which libyaml readline zlib openssl libxml2 libxslt libiconv postgresql curl ];
-  #     extraCmds = ''
-  #       unset http_proxy
-  #       unset ftp_proxy
+    # # \curl -L https://get.rvm.io | bash -s stable --rails --auto-dotfiles
+    # # gem install bundle
+    # # gem install pg
+    # # gem install nokogiri -- --use-system-libraries
+    # # bundle config build.nokogiri --use-system-libraries
+    # # bundle install
 
-  #       # fix curl
-  #       export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+    # rvmEnv = pkgs.myEnvFun {
+    #   name = "rvm";
+    #   buildInputs = [ stdenv pkgconfig python pythonPackages.boto procps which libyaml readline zlib openssl libxml2 libxslt libiconv postgresql curl ];
+    #   extraCmds = ''
+    #     unset http_proxy
+    #     unset ftp_proxy
 
-  #       # start postgresql
-  #       export PGDATA=~/.mytmp/pgdata
-  #       # initdb
-  #       # pg_ctl start
-  #       # createdb nuuz_dev
+    #     # fix curl
+    #     export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
 
-  #       export LIBXML2_DIR=${libxml2}
-  #       export LIBXSLT_DIR=${libxslt}
-  #       export LIBICONV_DIR=${libiconv}
-  #       export OPENSSL_DIR=${openssl}
-  #       export READLINE_DIR=${readline}
-  #       export LIBYAML_DIR=${libyaml}
-  #       export LOCAL_REDIS=true
-  #       . ~/.rvm/scripts/rvm
-  #     '';
-  #   };
+    #     # start postgresql
+    #     export PGDATA=~/.mytmp/pgdata
+    #     # initdb
+    #     # pg_ctl start
+    #     # createdb nuuz_dev
 
-  # };
+    #     export LIBXML2_DIR=${libxml2}
+    #     export LIBXSLT_DIR=${libxslt}
+    #     export LIBICONV_DIR=${libiconv}
+    #     export OPENSSL_DIR=${openssl}
+    #     export READLINE_DIR=${readline}
+    #     export LIBYAML_DIR=${libyaml}
+    #     export LOCAL_REDIS=true
+    #     . ~/.rvm/scripts/rvm
+    #   '';
+    # };
+
+  };
 }
--- zsh/docker-ruby.sh
+++ zsh/docker-ruby.sh
@@ -8,3 +8,14 @@ docker-ruby () {
     ruby:2.3 \
     "$@"
 }
+
+docker-python () {
+  docker run \
+    -it \
+    --rm \
+    --name python3 \
+    -v `pwd`:/usr/src/app \
+    -w /usr/src/app \
+    python:3 \
+    "$@"
+}
