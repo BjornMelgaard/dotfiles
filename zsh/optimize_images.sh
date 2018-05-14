7cef17bcd549d13477dff51d3c8ccd3c06e6b6b6
BjornMelgaard <melgaardbjorn@gmail.com>
2018-05-14 13:48:54 +0300

    feat: optimize_images -> size as argument

--- zsh/optimize_images.sh
+++ zsh/optimize_images.sh
@@ -1,11 +1,16 @@
 optimize_images () {
+  size=$1
+
   echo "Optimizing dir $PWD"
 
   docker run -it --rm --name optimize_images_foo -v $PWD:/usr/src/app -w /usr/src/app ruby:2.4-stretch bash -c 'gem install image_optim image_optim_pack && image_optim --allow-lossy --jpegoptim-allow-lossy true --jpegoptim-max-quality 85 --pngquant-allow-lossy true --pngquant-quality 0..85 -r .'
 
   echo "Done optimizing dir $PWD"
 
-  find "$PWD" -regex '.*\.\(jpg\|jpeg\|png\)' -exec sh -c 'identify -format "%[fx:(h>1000 || w>1000)]\n" "$0" | grep -q 1' {} \; -print0 | xargs -0 mogrify -verbose -resize '1000x1000'
+  resize="$size""x""$size"
+  cmd="identify -format \"%[fx:(h>$size || w>$size)]\n\" | grep -q 1"
+
+  find "$PWD" -regex '.*\.\(jpg\|jpeg\|png\)' -exec sh -c "$cmd" {} \; -print0 | xargs -0 mogrify -verbose -resize "$resize"
 
   echo "Done resizing dir $PWD"
 }
