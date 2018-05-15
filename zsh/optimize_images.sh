bc510ec54e50322219ec9d799243b3f764139352
BjornMelgaard <melgaardbjorn@gmail.com>
2018-05-15 19:20:32 +0300

    feat: optimize_images -> swap args

--- zsh/optimize_images.sh
+++ zsh/optimize_images.sh
@@ -1,4 +1,4 @@
-optimize_images () {
+resize_and_optimize_images () {
   size=$1
 
   resize="$size""x""$size"
@@ -10,12 +10,16 @@ optimize_images () {
 
   echo "Done resizing dir $PWD"
 
+  optimize_images $PWD 85
+}
 
-  quality='50'
+optimize_images () {
+  quality="$1"
+  dir="$2"
 
-  echo "Optimizing dir $PWD, quality - $quality"
+  echo "Optimizing dir $dir, quality - $quality"
 
-  docker run -it --rm --name optimize_images_foo -v $PWD:/usr/src/app -w /usr/src/app ruby:2.4-stretch bash -c "gem install image_optim image_optim_pack && (curl -L \"http://static.jonof.id.au/dl/kenutils/pngout-20150319-linux.tar.gz\" | tar -xz -C /usr/bin --strip-components 2 --wildcards \"*/x86_64/pngout\") && image_optim --allow-lossy --jpegoptim-allow-lossy true --jpegoptim-max-quality $quality --pngquant-allow-lossy true --pngquant-quality 0..$quality -r ."
+  docker run -it --rm --name optimize_images_foo -v $dir:/usr/src/app -w /usr/src/app ruby:2.4-stretch bash -c "gem install image_optim image_optim_pack && (curl -L \"http://static.jonof.id.au/dl/kenutils/pngout-20150319-linux.tar.gz\" | tar -xz -C /usr/bin --strip-components 2 --wildcards \"*/x86_64/pngout\") && image_optim --verbose --allow-lossy --jpegoptim-allow-lossy true --jpegoptim-max-quality $quality --pngquant-allow-lossy true --pngquant-quality 0..$quality -r ."
 
-  echo "Done optimizing dir $PWD"
+  echo "Done optimizing dir $dir"
 }
