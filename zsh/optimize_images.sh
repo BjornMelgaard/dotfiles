65ce5069ef27a3ca89769ff95817d56e01cfea15
BjornMelgaard <melgaardbjorn@gmail.com>
2018-05-16 11:59:13 +0300

    feat: optimize_images -> update

--- zsh/optimize_images.sh
+++ zsh/optimize_images.sh
@@ -1,6 +1,6 @@
 resize_and_optimize_images () {
   resize_images 500 $PWD
-  optimize_images 85 $PWD
+  optimize_images 100 $PWD
 }
 
 resize_images () {
@@ -13,11 +13,11 @@ resize_images () {
 
   for f in $dir/**/*.jpg $dir/**/*.jpeg $dir/**/*.png ; do
     echo "Checking $f"
-    s=`identify -format "%w" $f`
+    s=`identify -format "%w" "$f"`
 
     if [ $s -gt $max ]; then
       echo "Resizing..."
-      mogrify -verbose -resize $max $f
+      mogrify -verbose -resize "$max" "$f"
     fi
     echo
   done
@@ -31,12 +31,12 @@ optimize_images () {
 
   echo "Optimizing dir $dir, quality - $quality"
 
-  docker run -it --rm --name optimize_images_foo \
+  docker run -it --rm \
     -v $dir:/usr/src/app \
     -w /usr/src/app ruby:2.4-stretch bash -c \
     "gem install image_optim image_optim_pack && \
     (curl -L \"http://static.jonof.id.au/dl/kenutils/pngout-20150319-linux.tar.gz\" | tar -xz -C /usr/bin --strip-components 2 --wildcards \"*/x86_64/pngout\") && \
-    image_optim --verbose --allow-lossy --jpegoptim-allow-lossy true --jpegoptim-max-quality $quality --pngquant-allow-lossy true --pngquant-quality 0..$quality -r ."
+    image_optim --no-threads --verbose --allow-lossy --jpegoptim-allow-lossy true --jpegoptim-max-quality $quality --pngquant-allow-lossy true --pngquant-quality 0..$quality -r ."
 
   echo "Done optimizing dir $dir"
 }
