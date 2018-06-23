ec886179e03c608f94514dffa559ed5271f97edf
srghma <srghma@gmail.com>
2018-06-23 13:13:38 +0300

    refactor: users

--- nixos/root/users/default.nix
+++ nixos/root/users/default.nix
@@ -1,22 +1,25 @@
 { pkgs }:
 
 with pkgs;
-with (import /etc/nixos/metaconfiguration.nix);
 
 rec {
   mutableUsers = false;
+
   defaultUserShell = zsh;
+
   extraUsers =
-    # generated via: mkpasswd -m sha-512
-    let hashedPassword = "$6$Xne/w.j0RkrN$lRcThoZiNea2YBhfbeVJ2dgylt6Ov0IZbh1bf3flR6DYNpnv8YTTPJGwhk/8XPGNkgIrlKtXfvBowCZ7nbVt6/";
-  in {
-    root = {
-      inherit hashedPassword;
+    let
+      # generated via: mkpasswd -m sha-512
+      hashedPassword = "$6$Xne/w.j0RkrN$lRcThoZiNea2YBhfbeVJ2dgylt6Ov0IZbh1bf3flR6DYNpnv8YTTPJGwhk/8XPGNkgIrlKtXfvBowCZ7nbVt6/";
+    in {
+      root = {
+        inherit hashedPassword;
+      };
+
+      srghma = {
+        isNormalUser = true;
+        extraGroups = [ "audio" "disk" "wheel" "networkmanager" "docker" "vboxusers" "libvirtd" ];
+        inherit hashedPassword;
+      };
     };
-    ${userName} = {
-      isNormalUser = true;
-      extraGroups = [ "audio" "disk" "wheel" "networkmanager" "docker" "vboxusers" "libvirtd" ];
-      inherit hashedPassword;
-    };
-  };
 }
