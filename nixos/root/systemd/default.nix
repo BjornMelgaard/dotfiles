6efe212fbb6bf651128d6f361e65d4da89cf69c6
srghma <srghma@gmail.com>
2018-06-23 13:10:51 +0300

    refactor: systemd services

--- nixos/root/systemd/default.nix
+++ nixos/root/systemd/default.nix
@@ -1,32 +1,8 @@
 { config, pkgs }:
 
-# ${pkgs.xorg.xinput}/bin/xinput --disable 'ELAN1300:00 04F3:3057 Touchpad'
-# ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${dotfilesDir}/layouts/en_ru $DISPLAY
-
 with pkgs;
-with (import /etc/nixos/metaconfiguration.nix);
-
-let
-  touchpadId = "ELAN1300:00 04F3:3057 Touchpad";
-in
 
 rec {
-  # TODO: QtFatal: This application failed to start because it could not find or load the Qt platform
-  # user.services.copyq = {
-  #   description = "Copyq";
-
-  #   wantedBy = [ "graphical-session.target" ];
-  #   partOf = [ "graphical-session.target" ];
-
-  #   serviceConfig = {
-  #     ExecStart = ''
-  #       ${copyq}/bin/copyq
-  #     '';
-  #     RestartSec = 3;
-  #     Restart = "always";
-  #   };
-  # };
-
   user.services.disable-touchpad = {
     description = "Disable touchpad";
 
@@ -34,31 +10,19 @@ rec {
     partOf = [ "graphical-session.target" ];
 
     serviceConfig = {
-      ExecStart = ''
-        ${xorg.xinput}/bin/xinput --disable '${touchpadId}'
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+
+      ExecStart = pkgs.writeScript "disable-touchpad" ''
+        #! ${pkgs.stdenv.shell} -el
+
+        TOUCHPAD_NAME="touchpad"
+        TOUCHPAD_ID=$(echo $(xinput list) | sed -n "s/.*$TOUCHPAD_NAME.*id=\([0-9]\+\).*$/\1/gIp")
+
+        if [ -n "$TOUCHPAD_ID" ]; then
+          ${xorg.xinput}/bin/xinput --disable "$TOUCHPAD_ID"
+        fi
       '';
-      RestartSec = 3;
-      Restart = "always";
     };
   };
-
-  # TODO: invalid command, maybe because of -I
-  #services.custom-layout = {
-  #  description = "Custom layout";
-  #  serviceConfig = {
-  #    Type = "simple";
-  #    User = userName;
-  #    ExecStart = writeScript "custom-layout" ''
-  #        #! ${bash}/bin/bash
-  #        set -xe
-  #        ${xorg.xkbcomp}/bin/xkbcomp -I${layout} - :0.0
-  #      '';
-  #    RemainAfterExit = "yes";
-  #    RestartSec = 3;
-  #    Restart = "always";
-  #  };
-  #  environment = { DISPLAY = ":${toString config.services.xserver.display}"; };
-  #  wantedBy = [ "display-manager.service" ];
-  #  after = [ "display-manager.service" ];
-  #};
 }
