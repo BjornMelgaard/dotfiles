550201dab4f7a8677d549c2358ca302d8a86bada
srghma <srghma@gmail.com>
2018-10-28 13:11:23 +0200

    feat: update

--- nixos/root/fonts/default.nix
+++ nixos/root/fonts/default.nix
@@ -10,10 +10,15 @@ rec {
     powerline-fonts
     font-awesome-ttf
     source-code-pro
-    nerdfonts
-    # hasklig
     noto-fonts
     emojione
+
+    # the font package loads very slow (https://github.com/NixOS/nixpkgs/issues/47921)
+    # to prevent error - download for github manually from
+    # https://github.com/ryanoasis/nerd-fonts/archive/2.0.0.tar.gz
+    # and then
+    # nix-prefetch-url file:///home/$USER/Downloads/nerd-fonts-2.0.0.tar.gz
+    # nerdfonts
   ];
 
   fontconfig = {
