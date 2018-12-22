edc53b1866b7dd8b3ed9fca3a2870967eda49349
Sergei Khoma <srghma@gmail.com>
2018-12-22 23:15:47 +0200

    feat: update

--- nixos/pkgs/cachix/default.nix
+++ nixos/pkgs/cachix/default.nix
@@ -1,12 +1,22 @@
-{ fetchFromGitHub, readRevision, preventImplicitDepsFromBeingGarbagecollected, ... }:
+{ fetchFromGitHub, ... }:
 
 let
-  src = fetchFromGitHub (
-    readRevision ./revision.json
-  );
+  revData = builtins.fromJSON (builtins.readFile ./revision.json);
+
+  url     = revData.url;
+
+  m       = builtins.match "https?://.*/(.*)/(.*)" url;
+  owner   = builtins.elemAt m 0;
+  repo    = builtins.elemAt m 1;
+
+  src = fetchFromGitHub {
+    inherit owner repo;
+    inherit (revData) rev sha256;
+  };
 
   drv = import src {};
 in
   # TODO
   # preventImplicitDepsFromBeingGarbagecollected [src] drv
+  # src
   drv
--- nixos/pkgs/kb-light/default.nix
+++ nixos/pkgs/kb-light/default.nix
@@ -1,7 +1,18 @@
-{ callPackage, fetchFromGitHub, readRevision }:
+{ callPackage, fetchFromGitHub, ... }:
 
-callPackage (
-  fetchFromGitHub (
-    readRevision ./revision.json
-  )
-) {}
+let
+  revData = builtins.fromJSON (builtins.readFile ./revision.json);
+
+  url     = revData.url;
+
+  m       = builtins.match "https?://.*/(.*)/(.*)" url;
+  owner   = builtins.elemAt m 0;
+  repo    = builtins.elemAt m 1;
+
+  src = fetchFromGitHub {
+    inherit owner repo;
+    inherit (revData) rev sha256;
+  };
+in
+  callPackage src { }
+  # src
