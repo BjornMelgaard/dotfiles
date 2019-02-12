9e0fb753e00f5b207d591be3b74448c52a93f55f
Serhii Khoma <srghma@gmail.com>
2019-02-12 22:05:25 +0200

    feat: update

--- nixos/pkgs/hubstaff/default.nix
+++ nixos/pkgs/hubstaff/default.nix
@@ -1,12 +1,11 @@
 { fetchFromGitHub, readRevision, ... }:
-# TODO: https://github.com/NixOS/nixpkgs/issues/38988
 
 let
-  nixpkgs = fetchFromGitHub (
-    readRevision ./revision.json
-  );
+  # nixpkgs = fetchFromGitHub (
+  #   readRevision ./revision.json
+  # );
 
-  # nixpkgs = /home/srghma/projects/nixpkgs;
+  nixpkgs = /home/srghma/projects/nixpkgs;
 
   nixpkgs-with-working-hubstaff = import nixpkgs { config = { allowUnfree = true; }; };
 in
