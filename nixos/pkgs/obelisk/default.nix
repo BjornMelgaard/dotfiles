eb87b534a2bf169b10c5bc49013edea3227cb1fe
Serhii Khoma <srghma@gmail.com>
2019-06-18 10:19:00 +0300

    feat: obelisk

--- nixos/pkgs/obelisk/default.nix
+++ nixos/pkgs/obelisk/default.nix
@@ -1,8 +1,8 @@
 { lib, callPackage, fetchFromGitHub, readRevision, addAsRuntimeDeps }:
 
 let
-  # src = fetchFromGitHub (readRevision ./revision.json);
-  src = fetchTarball "https://github.com/obsidiansystems/obelisk/archive/master.tar.gz";
+  src = fetchFromGitHub (readRevision ./revision.json);
+  # src = fetchTarball "https://github.com/obsidiansystems/obelisk/archive/master.tar.gz";
 
   # drv = callPackage src {};
   drv = import src {};
--- nixos/pkgs/obelisk/update.sh
+++ nixos/pkgs/obelisk/update.sh
@@ -2,4 +2,4 @@
 #!nix-shell -i bash -p nix-prefetch-git
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
-nix-prefetch-git https://github.com/obsidiansystems/obelisk > "$SCRIPT_DIR/revision.json"
+nix-prefetch-git https://github.com/obsidiansystems/obelisk --rev refs/heads/master > "$SCRIPT_DIR/revision.json"
