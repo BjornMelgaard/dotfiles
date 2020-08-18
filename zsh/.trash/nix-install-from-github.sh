cd77dabc88a4eec198c1bb6b3f1164b652723b3e
Serhii Khoma <srghma@gmail.com>
2020-08-18 20:44:43 +0300

    feat: update

--- zsh/.trash/nix-install-from-github.sh
+++ zsh/.trash/nix-install-from-github.sh
@@ -1,12 +1,12 @@
-clone-and-cd () {
+git-clone-and-cd () {
   cd $1
   git clone $2
-  cd $(ls -tU | head -n 1)
+  cd $1/$(basename "$2")
 }
 
 nix-install-from-github () {
   olddir=`pwd`
-  clone-and-cd /tmp $1
+  git-clone-and-cd /tmp $1
   nix-env -f . -i
   cd $olddir
 }
