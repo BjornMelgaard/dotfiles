87ef1be21c89a7ab332c251afe429cc3d68d4ed8
Serhii Khoma <srghma@gmail.com>
2020-09-07 13:54:34 +0300

    feat: update

--- nixos/pkgs/hubstaff/update.sh
+++ nixos/pkgs/hubstaff/update.sh
@@ -2,4 +2,4 @@
 #!nix-shell -i bash -p nix-prefetch-git
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
-nix-prefetch-git https://github.com/srghma/nixpkgs --rev refs/heads/master --no-deepClone > "$SCRIPT_DIR/revision.json"
+nix-prefetch-git https://github.com/srghma/nixpkgs --rev refs/heads/hubstaff_update --no-deepClone > "$SCRIPT_DIR/revision.json"
