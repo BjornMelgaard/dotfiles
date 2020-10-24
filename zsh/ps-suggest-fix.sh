188ac6855ee97b57244a8b0ceceafd8ddba3145d
Serhii Khoma <srghma@gmail.com>
2020-10-24 19:57:29 +0300

    feat: update

--- zsh/ps-suggest-fix.sh
+++ zsh/ps-suggest-fix.sh
@@ -7,7 +7,7 @@ ps-suggest-fix () {
   # remove dir to force purs output ALL warnings for ALL files
   rm -fdR ./output
 
-  eval "spago $1 build --purs-args \"--json-errors\"" 2>$tmpfile
+  eval "spago $1 build --purs-args \"--json-errors\"" 1>$tmpfile
 
   cat $tmpfile | ps-suggest --list
 
